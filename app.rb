83e1136fad3521ff3958c04fc66eafa2a211a6d5
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-18 23:51:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "opihikao"
+  end
+
+  get '/' do
+    erb :index
+  end
 end
